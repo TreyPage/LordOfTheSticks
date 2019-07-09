098e114e8086706cb7e97a5aa4a48aa46caaf82c
Terrell Page <tpage46@gmail.com>
2019-07-09 15:33:47 -0600

    implemented joysticks

--- app/src/main/java/edu/cnm/deepdive/lordofthesticks/GamePlay.java
+++ app/src/main/java/edu/cnm/deepdive/lordofthesticks/GamePlay.java
@@ -1,13 +1,39 @@
 package edu.cnm.deepdive.lordofthesticks;
 
+import android.graphics.PixelFormat;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import edu.cnm.deepdive.lordofthesticks.JoystickView.JoystickListener;
 
-public class GamePlay extends AppCompatActivity {
+public class GamePlay extends AppCompatActivity implements JoystickView.JoystickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+//    JoystickView joystick = new JoystickView(this);
     setContentView(R.layout.activity_game_play);
+    JoystickView joystickLeft = findViewById(R.id.joystickLeft);
+    JoystickView joystickRight = findViewById(R.id.joystickRight);
+    joystickLeft.setZOrderOnTop(true);
+    joystickRight.setZOrderOnTop(true);
+    SurfaceHolder surfaceRight = joystickRight.getHolder();
+    SurfaceHolder surfaceLeft = joystickLeft.getHolder();
+    surfaceRight.setFormat(PixelFormat.TRANSPARENT);
+    surfaceLeft.setFormat(PixelFormat.TRANSPARENT);
+  }
+
+  @Override
+  public void onJoystickMoved(float xPercent, float yPercent, int id) {
+    switch (id)
+    {
+      case R.id.joystickRight:
+        Log.d("Right Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+        break;
+      case R.id.joystickLeft:
+        Log.d("Left Joystick", "X percent: " + xPercent + " Y percent: " + yPercent);
+        break;
+    }
   }
 }

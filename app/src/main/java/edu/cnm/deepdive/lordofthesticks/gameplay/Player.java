fc6befbd2afd4e8ba652afd687a74e9c1282d088
John B <bohnjailey56@gmail.com>
2019-08-07 08:37:46 -0600

    Controls

--- app/src/main/java/edu/cnm/deepdive/lordofthesticks/gameplay/Player.java
+++ app/src/main/java/edu/cnm/deepdive/lordofthesticks/gameplay/Player.java
@@ -1,18 +1,3 @@
-/*
-Copyright 2019 Brian Alexander, John Bailey, Austin DeWitt, Trey Page
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
 package edu.cnm.deepdive.lordofthesticks.gameplay;
 
 import com.badlogic.gdx.physics.box2d.Body;
@@ -21,29 +6,18 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-/**
- * Class to define the Player character that the {@link edu.cnm.deepdive.lordofthesticks.model.User}
- * controllers throughout the game.
- */
+
 public class Player {
-
-
-
-  private static final int BOX_SIZE = 38;
+  private static final int BOX_SIZE = 40;
   private static final float PLAYER_DENSITY = 0.8f;
-  static final float JUMP_FORCE = 4500f;
-  static final float RUN_FORCE = 25f;
-  static final String PLAYER_IMG_PATH = "stickman.png";
+  public static final float JUMP_FORCE = 4500f;
+  public static final float RUN_FORCE = 4500f;
+  public static final String PLAYER_IMG_PATH = "stickman.png";
   private static final float PLAYER_START_X = 8f;
   private static final float PLAYER_START_Y = 18f;
   private Body body;
   private boolean isJumping = false;
   private boolean isDead = false;
-  private boolean isMoving = false;
-
-  public static int getBoxSize() {
-    return BOX_SIZE;
-  }
 
 
   public void hit() {
@@ -60,10 +34,7 @@ public class Player {
   }
 
 
-  /**
-   * Constructor to take in a {@link #createBoxBody(World, float, float)} object
-   * @param world {@link World}
-   */
+
   public Player(World world) {
     createBoxBody(world, PLAYER_START_X, PLAYER_START_Y);
   }
@@ -74,18 +45,13 @@ public class Player {
     bdef.type = BodyDef.BodyType.DynamicBody;
     bdef.position.set(x, y);
     PolygonShape shape = new PolygonShape();
-    shape.setAsBox(BOX_SIZE / StickTest.PIXEL_PER_METER / 1.1f , BOX_SIZE / StickTest.PIXEL_PER_METER * 2.4f);
+    shape.setAsBox(BOX_SIZE / StickTest.PIXEL_PER_METER / 1.5f , BOX_SIZE / StickTest.PIXEL_PER_METER * 2.4f);
     FixtureDef fixtureDef = new FixtureDef();
     fixtureDef.shape = shape;
     fixtureDef.density = PLAYER_DENSITY;
     body = world.createBody(bdef);
     body.createFixture(fixtureDef).setUserData(this);
   }
-
-  /**
-   * Getter for getting the {@link Body}
-   * @return {@link Body}
-   */
   public Body getBody() {
     return body;
   }
--- app/src/main/java/edu/cnm/deepdive/lordofthesticks/gameplay/StickTest.java
+++ app/src/main/java/edu/cnm/deepdive/lordofthesticks/gameplay/StickTest.java
@@ -1,27 +1,13 @@
-/*
-Copyright 2019 Brian Alexander, John Bailey, Austin DeWitt, Trey Page
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-*/
 package edu.cnm.deepdive.lordofthesticks.gameplay;
 
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
@@ -33,11 +19,6 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-/**
- * Class hold all of the logic for the game by creating the {@link Player} setting the view through
- * {@link #orthographicCamera} usage. By initially creating the {@link #world} then {@link Player} then rendering and unpdating
- * everything by pulling in the {@link Player} and {@link MapParser} to create the game.
- */
 public class StickTest extends ApplicationAdapter {
 
   private static final float SCALE = 1.5f;
@@ -53,11 +34,11 @@ public class StickTest extends ApplicationAdapter {
   private Viewport gamePort;
   private World world;
   private Player player;
-  private SpriteBatch batch;
+  public static SpriteBatch batch;
+  private Controller controller;
   private Texture texture;
   private OrthogonalTiledMapRenderer tiledMapRenderer;
   private TiledMap tiledMap;
-  private BitmapFont font;
 
   public static final int V_Height = 500;
   public static final int V_WIDTH = 1000;
@@ -71,31 +52,29 @@ public class StickTest extends ApplicationAdapter {
     world = new World(new Vector2(VELOCITY_X, VELOCITY_Y), false);
     batch = new SpriteBatch();
     texture = new Texture(Player.PLAYER_IMG_PATH);
+    controller = new Controller();
     box2DDebugRenderer = new Box2DDebugRenderer();
     tiledMap = new TmxMapLoader().load(MAP_PATH);
     tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
     MapParser.parseMapLayers(world, tiledMap);
     player = new Player(world);
     world.setContactListener(new WorldContactListener());
-
+    
   }
 
   @Override
   public void render() {
-    String str = "Hello World";
-    font = new BitmapFont();
-
     update();
     Gdx.gl.glClearColor(0.0f, 0.0f, 0f, 1f);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     tiledMapRenderer.render();
+    controller.draw();
     batch.setProjectionMatrix(orthographicCamera.combined);
+
     batch.begin();
-    font.draw(batch, str, 10, 10);
-    batch.end();
-    batch.begin();
+
     // All drawing goes in here! Will have to add in the crap here. The texture comes first, then x and y coordinates, then width then height.
-//    font.draw(batch, str, 10, 10);
+
     batch.draw(texture, player.getBody().getPosition().x * PIXEL_PER_METER - (texture.getWidth() / 4),
         player.getBody().getPosition().y * PIXEL_PER_METER - (texture.getHeight() / 4), 60f, 150f);
     batch.end();
@@ -122,6 +101,7 @@ public class StickTest extends ApplicationAdapter {
   @Override
   public void resize(int width, int height) {
     orthographicCamera.setToOrtho(false, width / SCALE, height / SCALE);
+    controller.resize(width, height);
   }
 
   @Override
@@ -133,32 +113,65 @@ public class StickTest extends ApplicationAdapter {
     tiledMapRenderer.dispose();
     tiledMap.dispose();
   }
-
   private void inputUpdate() {
-    int horizontalForce = 0;
-    boolean isJumping = false;
-    if (Gdx.input.isTouched()) {
-      Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-      touchPos = orthographicCamera.unproject(touchPos);
-      if (touchPos.x / PIXEL_PER_METER > player.getBody().getPosition().x)
-        horizontalForce += 1;
-      if (touchPos.x / PIXEL_PER_METER < player.getBody().getPosition().x)
-        horizontalForce -= 1;
-      if (touchPos.y / PIXEL_PER_METER > player.getBody().getPosition().y && !player.isJumping())
-        isJumping = true;
+//    if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+//      if (Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+//        player.getBody()
+//            .applyLinearImpulse(new Vector2(-200f, 0), player.getBody().getWorldCenter(), true);
+//      if (Gdx.input.isKeyPressed(Keys.CONTROL_RIGHT))
+//        player.getBody()
+//            .applyLinearImpulse(new Vector2(200f, 0), player.getBody().getWorldCenter(), true);
+
+
+//    if (Gdx.input.isKeyJustPressed(Keys.RIGHT)&& player.getBody().getLinearVelocity().x<= 7)
+//      player.getBody().applyLinearImpulse(new Vector2(0.1f, 0),player.getBody().getWorldCenter(),true);
+//    if (Gdx.input.isKeyJustPressed(Keys.UP))
+//      player.getBody().applyLinearImpulse(new Vector2(0,4f), player.getBody().getWorldCenter(),true);
+//    if (Gdx.input.isKeyJustPressed(Keys.RIGHT)&& player.getBody().getLinearVelocity().x<= 7)
+//      player.getBody().applyLinearImpulse(new Vector2(0.1f, 0),player.getBody().getWorldCenter(),true);
+//    if (Gdx.input.isKeyJustPressed(Keys.LEFT)&& player.getBody().getLinearVelocity().x<= -7)
+//      player.getBody().applyLinearImpulse(new Vector2(-0.1f, 0),player.getBody().getWorldCenter(),true);
+//  }
+
+
+    if (controller.isRightPressed()){
+      player.getBody()
+          .applyLinearImpulse(new Vector2(10, 0), player.getBody().getWorldCenter(), true);
     }
-    playerUpdate(horizontalForce, isJumping);
-  }
-
-
-  private void playerUpdate(int horizontalForce, boolean isJumping) {
-    if (player.isDead()) {
-      world.destroyBody(player.getBody());
-      player = new Player(world);
+    else if (controller.isLeftPressed())
+      player.getBody()
+          .applyLinearImpulse(new Vector2(-20f, 0), player.getBody().getWorldCenter(), true);
+    else
+      player.getBody().setLinearVelocity(new Vector2(0, player.getBody().getLinearVelocity().y));
+    if (controller.isUpPressed() && player.getBody().getLinearVelocity().y == 0)
+      player.getBody()
+          .applyLinearImpulse(new Vector2(0, 100f), player.getBody().getWorldCenter(), true);
     }
-    if(isJumping)
-      player.getBody().applyForceToCenter(0, Player.JUMP_FORCE, false);
-    player.getBody().setLinearVelocity(horizontalForce * Player.RUN_FORCE, player.getBody().getLinearVelocity().y);
-  }
+//  private void inputUpdate() {
+//    int horizontalForce = 0;
+//    boolean isJumping = false;
+//    if (Gdx.input.isTouched()) {
+//      Vector3 touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+//      touchPos = orthographicCamera.unproject(touchPos);
+//      if (touchPos.x / PIXEL_PER_METER > player.getBody().getPosition().x)
+//        horizontalForce += 1;
+//      if (touchPos.x / PIXEL_PER_METER < player.getBody().getPosition().x)
+//        horizontalForce -= 1;
+//      if (touchPos.y / PIXEL_PER_METER > player.getBody().getPosition().y && !player.isJumping())
+//        isJumping = true;
+//    }
+//    playerUpdate(horizontalForce, isJumping);
+//  }
 
-}
+    private void playerUpdate ( int horizontalForce, boolean isJumping){
+      if (player.isDead()) {
+        world.destroyBody(player.getBody());
+        player = new Player(world);
+      }
+      if (isJumping)
+        player.getBody().applyForceToCenter(0, Player.JUMP_FORCE, false);
+      player.getBody().setLinearVelocity(horizontalForce * Player.RUN_FORCE,
+          player.getBody().getLinearVelocity().y);
+    }
+
+  }
